f8cf6728723cc3ff7a777d2d8d586f798d7ef049
FardeenB2 <fardeenbhuiyan@fardeens-air.mynetworksettings.com>
2025-01-30 01:19:58 -0500

    Change home.html to index.html
    changed every home.html syntax to be called index.html for better deployment through github.

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-const menu = document.querySelector('#mobile-menu');        //calls id mobile-menu from home.html
+const menu = document.querySelector('#mobile-menu');        //calls id mobile-menu from index.html
 const menuLinks = document.querySelector('.navbar__menu');      //assigns this menuLinks variable to be the container menu of navigation bar.
 
 menu.addEventListener('click', function() {     //allows button action of the toggle menu button when it percieves a 'click'
